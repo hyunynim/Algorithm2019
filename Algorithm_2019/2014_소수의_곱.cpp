f69d91880121c54486918db20f38fdfa4e7e692b
Honghyun Ahn <hyunynim1@naver.com>
2020-02-13 16:22:07 +0900

    change dir

--- Algorithm_2019/2014_소수의_곱.cpp
+++ Algorithm_2019/2014_소수의_곱.cpp
@@ -5,24 +5,31 @@ int main() {
 	int k, n;
 	scanf("%d %d", &k, &n);
 	vector<ll> seq(k);
-	priority_queue<int> q;
+	unordered_map<ll, int> chk;
+	priority_queue<ll, vector<ll>, greater<ll>> q;
 	for (int i = 0; i < k; ++i) {
 		scanf("%lld", &seq[i]);
-		q.push(-seq[i]);
+		q.push(seq[i]);
+		chk[seq[i]] = 1;
 	}
-	ll lim = seq.back() * seq.back();
+	ll lim = seq[0];
 	while (1) {
-		ll cur = -q.top();
-		q.pop(); --n;
+		ll cur = q.top(); q.pop(); --n;
+		if (q.size() > 20000) {
+			while (n && q.size() && q.top() < lim) {
+				cur = q.top(); --n; q.pop();
+			}
+		}
 		if (n == 0) {
 			printf("%lld", cur);
 			return 0;
 		}
 		for (int i = 0; i < seq.size(); ++i) {
-			if (cur * seq[i] < (1LL << 31))
-				q.push(-(cur * seq[i]));
-			if (cur % seq[i] == 0)
-				break;
+			if (cur * seq[i] < (1LL << 31) && chk[cur * seq[i]] == 0) {
+				chk[cur * seq[i]] = 1;
+				q.push(cur * seq[i]);
+			}
 		}
+		lim *= seq[0];
 	}
 }
